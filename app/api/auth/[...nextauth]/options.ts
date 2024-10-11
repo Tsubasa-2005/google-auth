fc1b5aa3be94eb469720e100c6e0ded626dc611d
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-10-11 09:37:30 +0900

    :sparkles: nextauth

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,34 @@
+import type {NextAuthOptions} from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const options: NextAuthOptions = {
+    debug:true,
+    session: {
+        strategy: "jwt",
+        maxAge: 60 * 5,
+    },
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        async jwt({ token, account }) {
+            if (account) {
+                token.accessToken = account.id_token;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            return {
+                ...session,
+                accessToken: token.accessToken,
+                user: {
+                    ...session.user,
+                },
+            };
+        },
+    }
+};
