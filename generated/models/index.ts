98c35dd4a067ba2c0efbdd65a1ace0eca4612cab
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-10-11 09:34:11 +0900

    :books: code gen

---
+++ generated/models/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './GetUsers200Response';
+export * from './GoogleAuth401Response';
+export * from './SignInRequest';
+export * from './User';
