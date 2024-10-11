e423c8cd2ca41e8ae51204d2bddef4b5c2b44b2c
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-10-11 09:58:31 +0900

    :zap: solve error: any

--- app/api/auth/google/route.ts
+++ app/api/auth/google/route.ts
@@ -6,7 +6,6 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
     try {
         const requestBody = await request.json();
-        console.log("Full Request Body:", requestBody);
 
         const { user, accessToken } = requestBody.session;
 
@@ -14,17 +13,13 @@ export async function POST(request: Request) {
             signInRequest: { email: user.email, name: user.name, imageURL: user.image, token: accessToken }
         };
 
-        console.log("Google Auth Request:", googleAuthRequest);
-
         const response = await apiClient.googleAuthRaw(googleAuthRequest);
 
         if (response.raw.ok) {
             const token = response.raw.headers.get("set-cookie");
 
             if (token) {
-                console.log("aaaaaaaaaa")
                 await setCookie(token);
-                console.log("aaaaabbbbbbbbbbbbbbbb")
                 return NextResponse.json({ success: true }, { status: 200 });
             } else {
                 return NextResponse.json({ error: 'トークンが存在しません。' }, { status: 500 });
@@ -33,10 +28,14 @@ export async function POST(request: Request) {
             const errorData = await response.raw.json();
             return NextResponse.json({ error: errorData.error || 'ログインに失敗しました' }, { status: response.raw.status });
         }
-    } catch (error: any) {
-        const errorResponseBody = await error.response.text();
-        const parsedBody = JSON.parse(errorResponseBody);
-        console.log("Error Response Body:", parsedBody.message);
-        return NextResponse.json({ message: parsedBody.message }, { status: error.response.status });
+    } catch (error) {
+        if (error instanceof Error && "response" in error) {
+            const responseError = error as { response: Response };
+            const errorResponseBody = await responseError.response.text();
+            const parsedBody = JSON.parse(errorResponseBody);
+            return NextResponse.json({ message: parsedBody.message }, { status: responseError.response.status });
+        }
+
+        return NextResponse.json({ message: '予期しないエラーが発生しました。' }, { status: 500 });
     }
 }
