07ff7b33f1e4fd8725d190a061f93aa39c3aabf2
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-10-11 09:56:41 +0900

    :zap: expiresAt simply

--- lib/utils/cookie.ts
+++ lib/utils/cookie.ts
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers';
 
 export async function setCookie(token: string) {
 
-    const expiresAt = new Date(Date.now() + 1 * 60 * 60 * 1000);
+    const expiresAt = new Date(Date.now() + 60 * 60 * 1000);
 
     cookies().set('session', token, {
         httpOnly: true,
