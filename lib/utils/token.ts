04a158ebc647f7a07e9c0c9e4989e6982687ddb1
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-10-11 09:56:06 +0900

    :sparkles: import UserTokenPayload type

--- lib/utils/token.ts
+++ lib/utils/token.ts
@@ -1,26 +1,16 @@
 import 'server-only';
 import jwt from 'jsonwebtoken';
+import {UserTokenPayload} from "@/lib/utils/payload_type";
 
-// JWTのペイロードの型を定義
-interface UserTokenPayload extends jwt.JwtPayload {
-    id: number;
-    name: string;
-    imageURL: string;
-}
-
-// 型を指定したデコード関数
 export function parseToken(token: string): UserTokenPayload | null {
     try {
-        // jwt.decode は署名検証なしでトークンをデコードします
         const decoded = jwt.decode(token);
 
-        // 型をチェックしてペイロードが期待される構造かどうかを確認
         if (!decoded || typeof decoded === 'string') {
             console.error("Invalid token");
             return null;
         }
 
-        // decoded が期待される UserTokenPayload の型かどうか確認
         return decoded as UserTokenPayload;
     } catch (error) {
         console.error("Error decoding token:", error);
